aec43c588966e51ef7ae9af20fc3d3cb0b1fee1d
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-04-01 13:51:52 +0530

    books,categories,authors, publishers and about controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,74 @@
 Rails.application.routes.draw do
+  get 'publishers/new'
+
+  get 'publishers/create'
+
+  get 'publishers/update'
+
+  get 'publishers/edit'
+
+  get 'publishers/destroy'
+
+  get 'publishers/index'
+
+  get 'publishers/show'
+
+  get 'authors/new'
+
+  get 'authors/create'
+
+  get 'authors/update'
+
+  get 'authors/edit'
+
+  get 'authors/destroy'
+
+  get 'authors/index'
+
+  get 'authors/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/edit'
+
+  get 'categories/destroy'
+
+  get 'books/new'
+
+  get 'books/create'
+
+  get 'books/update'
+
+  get 'books/update'
+
+  get 'books/edit'
+
+  get 'books/destroy'
+
+  get 'books/index'
+
+  get 'books/show'
+
+  get 'about/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
