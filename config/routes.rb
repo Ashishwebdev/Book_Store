d39cbb8448e0120f7308d5bb7b329945ad06e3a6
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-04-01 23:17:44 +0530

    Books showed and styled properly<%end%>

--- config/routes.rb
+++ config/routes.rb
@@ -69,6 +69,7 @@ Rails.application.routes.draw do
 
   get 'about/index'
 
+get 'about' => 'about#index' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
